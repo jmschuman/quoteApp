354ab50f466ec4f5db2f0b32385b57c0e4d8f510
Joey Schuman <joseph.schuman@yahoo.com>
2017-11-28 11:17:49 -0500

    outline of functions

--- quote/models.py
+++ quote/models.py
@@ -1,23 +1,22 @@
-from django.db import models # should it be  "from quote.db import models"
+from django.db import models
 
-class User(models.Model): # what does "models.Model" mean in this context
-    user_id = models.CharField(max_length = 5) # Primary Key. How long should max_length be?
+class User(models.Model):
+    # Auto Primary Key id
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     email = models.CharField(max_length = 30)
     username = models.CharField(max_length = 30)
     password = models.CharField(max_length = 30)
-    quotes_posted = models.IntegerField(default=0)
-    quotes_guessed = models.IntegerField(default=0)
+
 
 class Group(models.Model):
-    group_id =  models.CharField(max_length = 5) # Primary Key
+    # Auto Primary Key id
     group_name = models.CharField(max_length = 30)
     group_is_private = models.BooleanField(default = false)
 
 class Quote(models.model):
-    quote_id = models.CharField(max_length = 5) # Primary Key
-    group_id = models.ForiegnKey(Group, on_delete=models.CASCADE) # I'm still confused on the meaning of "on_delete=models.CASCADE"
+    # Auto Primary Key id
+    group_id = models.ForiegnKey(Group, on_delete=models.CASCADE)
     user_id = models.ForiegnKey(User, on_delete=models.CASCADE)
     quote = models.CharField(max_length = 200)
     quote_details = models.CharField(max_length = 200)
@@ -25,16 +24,16 @@ class Quote(models.model):
     post_date = models.DateTimeField('date posted')
 
 class Guess(models.Model):
-    guess_id = models.CharField(max_length = 5) # Primary Key
+    # Auto Primary Key id
     user_id =  models.ForiegnKey(User, on_delete=models.CASCADE)
     group_id = models.ForiegnKey(Group, on_delete=models.CASCADE)
     quote_id = models.ForiegnKey(Quote, on_delete=models.CASCADE)
     guess = models.CharField(max_length = 50)
     is_correct = models.BooleanField(default = false)
 
-class User_Membership(models.Model): # This was a related data table in the schema. Is it supposed to be a class in models?
-    user_id = models.ForiegnKey(User, on_delete=models.CASCADE)
-    group_id = models.ForiegnKey(Group, on_delete=models.CASCADE)
+class UserMembership(models.Model): 
+    user_id = models.ManyToMany(User)
+    group_id = models.ManyToMany(Group)
     is_admin = models.BooleanField(default = false)
 
 
